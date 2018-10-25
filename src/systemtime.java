e05dbc1bd13f93c2bc7ac43bbe2aa48fa9c2642d
mrvdark <m.r.v.leo.dark@gmail.com>
2018-10-25 14:45:44 +0700

    Done

---
+++ src/systemtime.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class systemtime {
+    public static void main( String[] args){
+        Date date = new Date();
+        System.out.println("now is: " + date);
+    }
+}
